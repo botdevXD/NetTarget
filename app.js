c3d897ae8502534fcdf45de98b82dd8da65a7fb0
botdevXD <54240316+botdevXD@users.noreply.github.com>
2022-04-12 23:07:25 +0100

    s

--- app.js
+++ app.js
@@ -355,7 +355,7 @@ try{
             })
             
             ExpressApp.ws("/server_socket", (Client, Request) => { // make a connection request to (url:443/server_socket);
-                Request.clientIp = Client._socket.remoteAddress;
+                Request.clientIp = Request.clientIp || Client._socket.remoteAddress;
                 Request.clientIp = Request.clientIp.replace("::ffff:", "");
 
                 console.log(Request.clientIp)
