e9c40314b04f14cc5a61af6b4b59c5ca81d7718c
botdevXD <54240316+botdevXD@users.noreply.github.com>
2022-04-15 16:56:46 +0100

    MethodsCasing

--- Dependencies/Container/Boot/BootHandler.js
+++ Dependencies/Container/Boot/BootHandler.js
@@ -1,8 +1,22 @@
 class BootHandler{
     HandleRequest = function(Request, Response){
-        return Response.send(JSON.stringify({
-            wow: true
-        }))
+        const RequestBody = Request.body;
+
+        switch (RequestBody.TargetType || ""){
+            case "TCP":
+                console.log(1)
+                break
+            case "UDP":
+                console.log(2)
+                break
+            case "HTTP":
+                console.log(3)
+                break
+            default:
+                console.log(4)
+                break
+        }
+
     }
 }
 
