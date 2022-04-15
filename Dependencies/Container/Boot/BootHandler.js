edc4fdea7d9155ce0469b586bf72ca9102d40c17
botdevXD <54240316+botdevXD@users.noreply.github.com>
2022-04-15 19:56:06 +0100

    ez

--- Dependencies/Container/Boot/BootHandler.js
+++ Dependencies/Container/Boot/BootHandler.js
@@ -46,7 +46,19 @@ class BootHandler{
                 
                     break
                 case "STOP":
-
+                    if (typeof CurrentAttacks[Request.clientIP || "0.0.0.0.0"] != "undefined") {
+                        Response.status(200)
+                        Response.json({
+                            success: false,
+                            error: "Attack stopped!"
+                        })
+                    }else{
+                        Response.status(400)
+                        Response.json({
+                            success: false,
+                            error: "You haven't submitted an attack!"
+                        })
+                    }
                 default:
                     break
             }
