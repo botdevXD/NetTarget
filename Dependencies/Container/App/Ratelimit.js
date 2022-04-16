e5898b141feaf15c267a42cc4c98974286bf4b5e
botdevXD <54240316+botdevXD@users.noreply.github.com>
2022-04-16 06:09:36 +0100

    yh

--- Dependencies/Container/App/Ratelimit.js
+++ Dependencies/Container/App/Ratelimit.js
@@ -1,31 +1,37 @@
 class RateLimiter{
     constructor(Options){
-        const RequestHandler = this.RequestHandler
+        const RequestHandler = this.RequestHandler;
 
         Options = {
-            TotalRequests: 0,
+            url_data: {},
             max_requests: (typeof Options.max_requests == "number") ? Options.max_requests : 100,
             reset_time: (typeof Options.reset_time == "number") ? Options.reset_time : 60000,
             message: (typeof Options.message != "undefined" & Options.message != null) ? Options.message : "You've been rate limited!",
             endpoints: (typeof Options.endpoints == "object") ? Options.endpoints : {}
-        }
+        };
 
         return {
             function: function(Request, Response, Next){
-                return RequestHandler(Options, Request, Response, Next)
+                return RequestHandler(Options, Request, Response, Next);
             }
-        }
-    }
+        };
+    };
 
     RequestHandler = function(Options, Request, Response, Next){
         if (Options){
             if (((typeof Options.endpoints[Request.url] != "undefined") ?  (typeof Options.endpoints[Request.url].rate_limit != "undefined" ? Options.endpoints[Request.url].rate_limit : false) : false) == true){
-                return Response.send(Options.message.toString())
-            }
-        }
+                console.log(Options)
 
-        return Next()
-    }
-}
+                Response.status(429);
+                Response.setHeader("RateLimit-Limit", Options.max_requests);
+                Response.setHeader("RateLimit-Remaining", Math.max(Options.max_requests - Options.TotalRequests, 0));
+                
+                return Response.send(Options.message.toString());
+            };
+        };
 
-module.exports = RateLimiter
+        return Next();
+    };
+};
+
+module.exports = RateLimiter;
