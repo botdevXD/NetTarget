47fa90716244c3fb0fb75b266b69b15ec75dca9b
botdevXD <54240316+botdevXD@users.noreply.github.com>
2022-04-16 11:17:33 +0100

    RLimit

--- Dependencies/Container/App/Ratelimit.js
+++ Dependencies/Container/App/Ratelimit.js
@@ -10,6 +10,10 @@ class RateLimiter{
             endpoints: (typeof Options.endpoints == "object") ? Options.endpoints : {}
         };
 
+        setTimeout(() => {
+            
+        }, Options.reset_time);
+
         return {
             function: function(Request, Response, Next){
                 return RequestHandler(Options, Request, Response, Next);
@@ -20,11 +24,20 @@ class RateLimiter{
     RequestHandler = function(Options, Request, Response, Next){
         if (Options){
             if (((typeof Options.endpoints[Request.url] != "undefined") ?  (typeof Options.endpoints[Request.url].rate_limit != "undefined" ? Options.endpoints[Request.url].rate_limit : false) : false) == true){
+                Options.url_data[Request.url] = (typeof Options.url_data[Request.url] != "undefined") ? Options.url_data[Request.url] : {}
+                Options.url_data[Request.url][Request.clientIp] = (typeof Options.url_data[Request.url][Request.clientIp] != "undefined") ? Options.url_data[Request.url][Request.clientIp] : {
+                    requests: 0
+                }
+
                 console.log(Options)
 
-                Response.status(429);
-                Response.setHeader("RateLimit-Limit", Options.max_requests);
-                Response.setHeader("RateLimit-Remaining", Math.max(Options.max_requests - Options.TotalRequests, 0));
+                if (Options.url_data[Request.url][Request.clientIp].requests >= Options.max_requests){
+                    Response.status(429);
+                    Response.setHeader("RateLimit-Limit", Options.max_requests);
+                    Response.setHeader("RateLimit-Remaining", Math.max(Options.max_requests - Options.TotalRequests, 0));
+                }else{
+                    Options.url_data[Request.url][Request.clientIp].requests += 1
+                }
                 
                 return Response.send(Options.message.toString());
             };
